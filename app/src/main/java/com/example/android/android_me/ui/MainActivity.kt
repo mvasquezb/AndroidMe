be37940914f19c48cb58a5a675ef6e410127d57a
Martín Vásquez <pedromvasquezb@gmail.com>
2017-08-10 01:23:23 -0500

    Add MainActivity

---
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.android.android_me.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.example.android.android_me.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
