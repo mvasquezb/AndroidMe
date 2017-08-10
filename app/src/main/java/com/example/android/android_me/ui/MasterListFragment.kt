ad9f98537179bac971f9dd38e301868fbd2ae727
Martín Vásquez <pedromvasquezb@gmail.com>
2017-08-10 02:10:17 -0500

    Add MasterListFragment.OnImageClickListener

--- app/src/main/java/com/example/android/android_me/ui/MasterListFragment.kt
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.kt
@@ -1,5 +1,6 @@
 package com.example.android.android_me.ui
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -15,12 +16,34 @@ import com.example.android.android_me.data.AndroidImageAssets
  */
 
 class MasterListFragment : Fragment() {
+    var mImageClickCallback: OnImageClickListener? = null
+
+    interface OnImageClickListener {
+        fun onImageSelected(position: Int)
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+
+        try {
+            mImageClickCallback = context as OnImageClickListener
+        } catch (ex: ClassCastException) {
+            throw ClassCastException(context.toString()
+                    + "must implement OnImageClickListener")
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 //        val rootView = container?.inflate(R.layout.fragment_master_list, inflater = inflater)
         val rootView = inflater?.inflate(R.layout.fragment_master_list, container, false)
         val gridView = rootView?.findViewById(R.id.master_list_grid) as GridView
 
         gridView.adapter = MasterListAdapter(context, AndroidImageAssets.all)
+
+        gridView.setOnItemClickListener { adapterView, view, position, l ->
+            mImageClickCallback?.onImageSelected(position)
+        }
+
         return rootView
     }
 }
